125d041806079a48a85954bb073c35e4e7e594bf
Kristen <sayyes566@gmail.com>
2018-11-03 09:07:56 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
 new Vue({
